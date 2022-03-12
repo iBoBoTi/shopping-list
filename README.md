b57036a42375af6f18df93d0be8b8c1fda5fe441
iBoBoTi <iroegbucalistus@gmail.com>
2022-03-12 23:27:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-qn9dvs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-qn9dvs)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    data(){
+      return {
+        greeting : "Hello VueJS"
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>{{ greeting }}</h1>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
