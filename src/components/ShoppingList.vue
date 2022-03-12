8637233a5b5f3608b0fc765f7d6c96b58a1630a8
iBoBoTi <iroegbucalistus@gmail.com>
2022-03-13 00:01:06 +0100

    1 file modified, 1 added, 1 deleted

---
+++ src/components/ShoppingList.vue
@@ -0,0 +1,38 @@
+<script>
+  export default {
+    data(){
+      return {
+        header : "Shopping List App",
+        newItem : "",
+        items : [
+          "10 bags of rice",
+          "fuel",
+          "Domino Ice-cream"
+        ],
+      }
+    },
+    methods:{
+      saveItem(){
+        if (this.newItem !== "")  {
+          this.items.push(this.newItem)
+          this.newItem = ""
+        }
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>{{ header.toLocaleUpperCase() }}</h1>
+  <input type="text" v-model="newItem" placeholder="Add an item" @keyup.enter="saveItem">
+  <button class="btn btn-primary" @click="saveItem">Save Item</button>
+  <ul>
+    <li v-for="item in items">{{ item }}</li>
+  </ul>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
